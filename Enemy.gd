4a5b52ddc8ac70d51987a2da05aac9185ee83515
Alex Wells <awells111@users.noreply.github.com>
2024-01-13 20:14:31 -0500

    enemy movement + rand spawn

--- Enemy.gd
+++ Enemy.gd
@@ -1,4 +1,7 @@
 extends CharacterBody2D
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 #func _ready(): position = get_parent().get_node("Player").position + Vector2(1000, 0).rotated(randf_range(0, 2*PI))
 #set_velocity((get_parent().get_node("Player").position - position).normalized() * $AnimatedSprite2D.speed_scale / delta)
 #move_and_slide()
@@ -10,9 +13,11 @@ var speed: float = 200  # Adjust the speed as needed
 var player: Node2D
 
 func _ready():
-	# Assume the player is a child node named "Player"
-	# player = get_node("/root/Node2D/Player")
+	animated_sprite_2d.play("default")
 	player = get_parent().get_node("Player")
+	
+	# Starting position of the enemy
+	position = player.position + Vector2(700, 0).rotated(randf_range(0, 2*PI))
 
 func _process(delta: float):
 	# Move the enemy towards the player
