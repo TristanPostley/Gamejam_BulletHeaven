1f04b6461f919a9676b8379a1c180512c30d48ae
Alex Wells <awells111@users.noreply.github.com>
2024-01-20 20:09:01 -0500

    Prompts can be pressed in any order

--- Scenes/Key Prompts/intro_prompts.gd
+++ Scenes/Key Prompts/intro_prompts.gd
@@ -2,19 +2,39 @@ extends KeyPromptResource
 
 @onready var animation_player = %AnimationPlayer
 
-var directions = ['up', 'left', 'down', 'right']
-var last_pressed_direction = ''
+const directions = ['up', 'left', 'down', 'right']
+var next_key_press = directions[0]
+var keys_pressed : Dictionary = {}
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed(directions[0]) && last_pressed_direction == '':
+	# Track which keys have been pressed
+	if Input.is_action_pressed(directions[0]):
+		keys_pressed[directions[0]] = true
+	elif Input.is_action_pressed(directions[1]):
+		keys_pressed[directions[1]] = true
+	elif Input.is_action_pressed(directions[2]):
+		keys_pressed[directions[2]] = true
+	elif Input.is_action_pressed(directions[3]):
+		keys_pressed[directions[3]] = true
+	
+	# Exit early if the player still has to press the next key
+	if !keys_pressed.has(next_key_press):
+		return
+	
+	# Give a new prompt as needed
+	if !keys_pressed.has(directions[1]):
 		$AnimationPlayer.play("a_press")
-		last_pressed_direction = directions[0]
-	if Input.is_action_pressed(directions[1]) && last_pressed_direction == directions[0]:
+		next_key_press = directions[1]
+		return
+	elif !keys_pressed.has(directions[2]):
 		$AnimationPlayer.play("s_press")
-		last_pressed_direction = directions[1]
-	if Input.is_action_pressed(directions[2]) && last_pressed_direction == directions[1]:
+		next_key_press = directions[2]
+		return
+	elif !keys_pressed.has(directions[3]):
 		$AnimationPlayer.play("d_press")
-		last_pressed_direction = directions[2]
-	if Input.is_action_pressed(directions[3]) && last_pressed_direction == directions[2]:
-		queue_free()
+		next_key_press = directions[3]
+		return
+	
+	# All of the prompts have been pressed, so free up this resource
+	queue_free()
